3a48874a022104f18de2fb5487cdff3dbf216636
Bulbul verma <bulbulverma6266@gmail.com>
2026-09-07 19:27:10 +0530

    Add openrelay TURN fallback servers for NAT traversal

--- src/controller/twilio.controller.ts
+++ src/controller/twilio.controller.ts
@@ -4,7 +4,26 @@ import twilio from 'twilio'
 const fallbackIceServers = [
   { urls: "stun:stun.l.google.com:19302" },
   { urls: "stun:stun1.l.google.com:19302" },
-  { urls: "stun:stun2.l.google.com:19302" }
+  { urls: "stun:stun2.l.google.com:19302" },
+  { urls: "stun:stun3.l.google.com:19302" },
+  { urls: "stun:stun4.l.google.com:19302" },
+  { urls: "stun:stun.services.mozilla.com" },
+  { urls: "stun:global.stun.twilio.com:3478" },
+  {
+    urls: "turn:openrelay.metered.ca:80",
+    username: "openrelayproject",
+    credential: "openrelayproject"
+  },
+  {
+    urls: "turn:openrelay.metered.ca:443",
+    username: "openrelayproject",
+    credential: "openrelayproject"
+  },
+  {
+    urls: "turn:openrelay.metered.ca:443?transport=tcp",
+    username: "openrelayproject",
+    credential: "openrelayproject"
+  }
 ]
 
 export const getTurnServer = async(req: Request, res: Response) =>{
